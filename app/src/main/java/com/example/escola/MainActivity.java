ebcf886dcbf19d407559b955e4ce6eeaae6c9907
Raquel Ribeiro <raquelg.ribeiro26@gmail.com>
2022-07-25 06:47:34 -0300

    Created calculating logic

--- app/src/main/java/com/example/escola/MainActivity.java
+++ app/src/main/java/com/example/escola/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.escola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtN1, edtN2;
+    TextView txtM,txtSit;
+    LinearLayout layResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtN1 = findViewById(R.id.edtN1);
+        edtN2 = findViewById(R.id.edtN2);
+        txtM = findViewById(R.id.txtM);
+        txtSit = findViewById(R.id.txtSit);
+        layResult = findViewById(R.id.layResult);
+    }
+
+    public void calculate(View view) {
+        float n1 = Float.parseFloat(edtN1.getText().toString());
+        float n2 = Float.parseFloat(edtN2.getText().toString());
+        float m = (n1 + n2) / 2;
+
+        txtM.setText(String.format("%.1f",m));
+
+        if (m < 5){
+            txtSit.setText(getString(R.string.strSitRp));
+        } else if (m < 7){
+            txtSit.setText(getString(R.string.strSitRc));
+        } else {
+            txtSit.setText(getString(R.string.strSitAp));
+        }
     }
 }
